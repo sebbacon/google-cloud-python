dee69558d9ab84101cc860a98c8a797b6cea90c7
Danny Hermes <daniel.j.hermes@gmail.com>
2016-09-12 15:47:27 -0700

    Test commit: inspect the env. vars. Travis uses.
    This is sniffing around towards #2277, to see how much info
    we can use to restrict the tests we run.

---
+++ scripts/print_travis.py
@@ -0,0 +1,9 @@
+import os
+
+our_keys = sorted(key for key in os.environ.keys()
+                  if 'travis' in key.lower())
+if our_keys:
+    for key in our_keys:
+        print('%s: %s' % (key, os.getenv(key)))
+else:
+    print('No keys here, sorry')
